724b6593181217164682c56468fb0fe25cb3ccb4
Iacopo Poli <iapoli@lighton.ai>
2022-11-14 15:41:31 +0100

    First draft

---
+++ lightonmuse-sagemaker-sdk/__init__.py
@@ -0,0 +1,11 @@
+from .api_requests import Analyse, Compare, Create, Embed, Select, Tokenize
+
+
+__all__ = [
+    "Analyse",
+    "Compare",
+    "Create",
+    "Embed",
+    "Select",
+    "Tokenize"
+]
---
+++ lightonmuse-sagemaker-sdk/api_requests.py
@@ -0,0 +1,406 @@
+import json
+from typing import Dict, List, Optional, Tuple, Union
+
+import boto3
+
+
+class BaseRequest:
+    def __init__(self, model: str):
+        self.model = model
+        self.content_type = "application/json"
+        self.runtime_sm_client = boto3.client("runtime.sagemaker")
+
+    def request(self, payload) -> dict:
+        # TODO: add endpoint to the payload here or elsewhere?
+        response = self.runtime_sm_client.invoke_endpoint(
+            EndpointName=self.model,
+            ContentType=self.content_type,
+            Body=json.dumps(payload),
+        )
+        # check if this is necessary, how the invoke behaves
+        return json.loads(response["Body"].read().decode("utf8"))
+
+
+class Create(BaseRequest):
+    """Create endpoint.
+    Parameters
+    ----------
+    model: str,
+        name of the model to use as intelligence engine.
+    """
+
+    def __init__(self, model: str = "lyra-fr"):
+        super().__init__(model=model)
+
+    def __call__(
+        self,
+        text: Union[str, List[str]],
+        n_tokens: int = 20,
+        n_completions: int = 1,
+        n_best: int = 1,
+        # sampling
+        mode: str = "nucleus",
+        temperature: float = 1,
+        p: float = 0.9,
+        k: int = 5,
+        # control
+        word_biases: Optional[Dict[str, float]] = None,
+        presence_penalty: float = 0.0,
+        frequency_penalty: float = 0.0,
+        stop_words: Optional[List[str]] = None,
+        # utilities
+        concat_prompt: bool = False,
+        return_logprobs: bool = False,
+        seed: Optional[int] = None,
+    ) -> Tuple[List, int, str]:
+        """
+        Parameters
+        -------------
+        text: Union[str, List[str]],
+            input prompt or list of input prompts.
+        n_tokens: int, default 20,
+            number of tokens to generate.
+        n_completions: int, default 1,
+            number of different completion proposals to return for each prompt.
+            :warning: You will be charged for the total number of tokens generated:
+            `n_completions * n_tokens`, stay reasonable!
+        n_best: int, default 1,
+            only return the `best_of` among `n_completions`. Completions are selected
+            according to how likely they are, summing the log-likelihood over all tokens generated.
+        # sampling
+        mode: str, default "nucleus",
+            how the model will decide which token to select at each step. Choose between:
+            - `"greedy"`: the model will always select the most likely token. This generation mode
+            is deterministic and only suited for applications in which there is a ground truth the
+            model is expected to return (e.g. question answering).
+            - `"nucleus"`: the model will only consider the most likely tokens with total
+            probability mass p. We recommend this setting for most applications.
+            - `"topk"`: the model will only consider the k most likely tokens.
+            - `"typical"`: the model will discard high probability tokens with low expected information content.
+        temperature: float, default 1.,
+            controls how risky will the model be in its choice of tokens. A temperature of 0
+            corresponds to greedy sampling; we recommend a value around 1 for most creative
+            applications, and closer to 0 when a ground truth exists.
+        p: float, default 0.9,
+            total probability mass of the most likely tokens considered when sampling in
+            nucleus mode.
+        k: int, default 5,
+            number of most likely tokens considered when sampling in top-k mode.
+        # control
+        word_biases: Optional[Dict[str, float]], default None,
+            bias the provided words to appear more or less often in the generated text. Values
+            should be comprised between -100 and +100, with negative values making words more
+            unlikely to occur. Extreme values such as -100 will completely forbid a word,
+            while values between 1-5 will make the word more likely to appear. We recommend playing
+            around to find a good fit for your use case. We recommend using presence and frequency
+            penalty when positively biasing words.
+        presence_penalty: float, default 0.,
+            How strongly should tokens be prevented from appearing again. This is a one-off penalty:
+            tokens will be penalized after their first appearance, but not more if they appear
+            repetitively -- use `frequency_penalty` if that's what you want instead.
+            Values closer to 1 encourage variation of the topics generated.
+        frequency_penalty: float, default 0.,
+            How strongly should tokens be prevented from appearing again if they have appeared
+            repetitively. Contrary to presence_penalty, this penalty scales with how often the
+            token already occurs. Values closer to 1 discourage repetition, especially useful in
+            combination with `word_biases`.
+        stop_words: Optional[List[str]], default None,
+            encountering any of the words in this list will halt generation immediately.
+        # utilities
+        concat_prompt: bool, default False,
+            whether the original prompt will be concatenated with the generated text.
+        return_logprobs: int, default 0,
+            whether the log-probabilities of the generated tokens are returned.
+        seed: Optional[int], default None,
+            make sampling deterministic by setting a seed used for random number generation.
+            Useful to perfectly reproduce `Create` calls.
+        Return
+        ------
+        outputs: list,
+            list of dicts containing the completions for the input `text`, together with scores
+            and other metadata.
+        cost: int,
+            cost for the generation completed.
+        request_id: str,
+            ID string for the request.
+        """
+        if mode not in ["greedy", "topk", "nucleus", "typical"]:
+            raise ValueError(
+                f"mode: {mode} is not valid. Use one of `greedy`, `topk`, `nucleus` or `typical`"
+            )
+        params = {
+            "n_tokens": n_tokens,
+            "n_completions": n_completions,
+            "n_best": n_best,
+            # sampling
+            "mode": mode,
+            "temperature": temperature,
+            "p": p,
+            "k": k,
+            # control
+            "biases": word_biases,
+            "presence_penalty": presence_penalty,
+            "frequence_penalty": frequency_penalty,
+            "stop_words": stop_words,
+            # utilities
+            "concat_prompt": concat_prompt,
+            "return_logprobs": return_logprobs,
+            "seed": seed,
+        }
+        payload = {"text": text, "params": params, "endpoint": "create"}
+        response = self.request(payload)
+        request_id = response["request_id"]
+        cost = response["costs"]
+        outputs = response["outputs"][0]
+        return outputs, cost, request_id
+
+
+class Analyse(BaseRequest):
+    """Analyse endpoint.
+    Parameters
+    ----------
+    model: str,
+        name of the model to use as intelligence engine.
+    """
+
+    def __init__(self, model: str = "lyra-fr"):
+        super().__init__(model=model)
+
+    def __call__(
+        self, text: Union[str, List[str]]
+    ) -> Tuple[List, int, str]:
+        """Parameters
+        -------------
+        text: Union[str, List[str]],
+            input text or list of input texts.
+        
+        Return
+        ------
+        outputs: list,
+            list of dicts containing the input `text`(s), together with scores
+            and other metadata.
+        cost: int,
+            cost for the analysis completed.
+        request_id: str,
+            ID string for the request.
+        """
+        payload = {"text": text, "endpoint": "analyse"}
+        response = self.request(payload)
+        request_id = response.get("request_id")
+        cost = response.get("costs")
+        outputs = response.get("outputs")[0]
+        return outputs, cost, request_id
+
+
+class Embed(BaseRequest):
+    """Embed endpoint.
+    Parameters
+    ----------
+    model: str,
+        name of the model to use as intelligence engine.
+    """
+    def __init__(self, model: str = "lyra-fr"):
+        super().__init__(model=model)
+
+    def __call__(
+        self, text: Union[str, List[str]]) -> Tuple[List, int, str]:
+        """Parameters
+        -------------
+        text: Union[str, List[str]],
+            input text or list of input texts.
+        
+        Return
+        ------
+        outputs: list,
+            list of dicts containing the input `text`(s), together with embeddings
+            and other metadata.
+        cost: int,
+            cost for the analysis completed.
+        request_id: str,
+            ID string for the request.
+        """
+        payload = {"text": text, "endpoint": "embed"}
+        response = self.request(payload)
+        request_id = response.get("request_id")
+        cost = response.get("costs")
+        outputs = response.get("outputs")[0]
+        return outputs, cost, request_id
+
+
+class Select(BaseRequest):
+    """Select endpoint.
+    Parameters
+    ----------
+    model: str,
+        name of the model to use as intelligence engine.
+    """
+
+    def __init__(self, model: str = "orion-fr-v2"):
+        super().__init__(model=model)
+
+    def __call__(
+        self,
+        reference: Union[str, List[str]],
+        candidates: Union[List[str], List[List[str]]],
+        evaluate_reference: bool = False,
+        conjunction: str = None,
+        concat_best: bool = False,
+    ) -> Tuple[List, int, str]:
+        """Parameters
+        -------------
+        reference: Union[str, List[str]],,
+            reference input or list of reference inputs to compute likelihood against.
+        candidates: Union[List[str], List[List[str]]],
+            input(s) that are compared to the reference and ranked based on likelihood.
+            If `candidates` is a list of lists, `reference` should be a list, and they should have the same length.
+            Each entry will be computed against the corresponding `reference` input.
+        evaluate_reference: bool, default to False,
+            if True, evaluates the loglikelihood of candidate given reference,
+            instead of reference given candidates. Especially useful when candidates
+            have very different lengths.
+        conjunction: str, default to None,
+            expression used to link `reference` and `candidates` to create the prompt used to
+            compute the likelihood. The prompt will have the structure
+            `reference + conjunction + candidate`. Finding a good conjunction can greatly increase
+            the performance of `Select`.
+        concat_best: bool, default to False,
+            whether the response will contain a "best" field with the selected choice.
+        Return
+        ------
+        outputs: list,
+            list of dicts containing the reference text, the rankings of the candidates together
+            with their scores and other metadata.
+        cost: int,
+            cost for the analysis completed.
+        request_id: str,
+            ID string for the request.
+        """
+        if isinstance(reference, str):
+            assert all(
+                isinstance(x, str) for x in candidates
+            ), "When `reference` is a string, `candidates` should be a list of strings."
+            payload_dict = {
+                "reference": reference,
+                "candidates": candidates,
+                "evaluate_reference": evaluate_reference,
+                "conjunction": conjunction,
+                "concat_best": concat_best,
+                "endpoint": "select"
+            }
+        elif isinstance(reference, list):
+            # different candidates for each reference
+            if all(isinstance(x, list) for x in candidates):
+                assert len(reference) == len(candidates), (
+                    f"`reference` and `candidates` should have the same length, "
+                    "got {len(reference)} and {len(candidates)} instead."
+                )
+                payload_dict = [
+                    {
+                        "reference": ref,
+                        "candidates": cand,
+                        "evaluate_reference": evaluate_reference,
+                        "conjunction": conjunction,
+                        "concat_best": concat_best,
+                        "endpoint": "select"
+                    }
+                    for ref, cand in zip(reference, candidates)
+                ]
+            # same candidates for each reference
+            elif all(isinstance(x, str) for x in candidates):
+                payload_dict = [
+                    {
+                        "reference": ref,
+                        "candidates": candidates,
+                        "evaluate_reference": evaluate_reference,
+                        "conjunction": conjunction,
+                        "concat_best": concat_best,
+                        "endpoint": "select"
+                    }
+                    for ref in reference
+                ]
+        else:
+            raise TypeError(
+                f"`reference` of type {type(reference)} is not supported, it should be `str` or `list`."
+            )
+        payload = payload_dict
+        response = self.request(payload)
+        request_id = response.get("request_id")
+        cost = response.get("costs")
+        outputs = response.get("outputs")
+        if len(outputs) == 1:
+            # for backward compatibility after adding batching support
+            outputs = outputs[0]
+        return outputs, cost, request_id
+
+
+class Compare(BaseRequest):
+    """Compare endpoint.
+    Parameters
+    ----------
+    model: str,
+        name of the model to use as intelligence engine.
+    """
+
+    def __init__(self, model: str = "lyra-fr"):
+        super().__init__(model=model)
+
+    def __call__(
+        self, reference: str, candidates: List[str]
+    ) -> Tuple[List, int, str]:
+        """Parameters
+        -------------
+        reference: str,
+            reference input to compute cosine similarity against.
+        candidates: List[str],
+            input(s) that are compared to the reference and ranked based on similarity.
+
+        Return
+        ------
+        outputs: list,
+            list of dicts containing the reference text, the rankings of the candidates together
+            with their scores and other metadata.
+        cost: int,
+            cost for the analysis completed.
+        request_id: str,
+            ID string for the request.
+        """
+        payload = {"reference": reference, "candidates": candidates, "endpoint": "compare"}
+        response = self.request(payload)
+        request_id = response.get("request_id")
+        cost = response.get("costs")
+        outputs = response.get("outputs")[0]
+        return outputs, cost, request_id
+
+
+class Tokenize(BaseRequest):
+    """Tokenize endpoint.
+    Parameters
+    ----------
+    model: str,
+        name of the model to use as intelligence engine.
+    """
+
+    def __init__(self, model: str = "lyra-fr"):
+        super().__init__(model=model)
+
+    def __call__(self, text: Union[str, List[str]]) -> Tuple[List, int, str]:
+        """Parameters
+        -------------
+        text: Union[str, List[str]],
+            input text or list of input texts.
+        Return
+        ------
+        outputs: list,
+            list of dicts containing the reference text, the rankings of the candidates together
+            with their scores and other metadata.
+        cost: int,
+            cost for the analysis completed.
+        request_id: str,
+            ID string for the request.
+        """
+        payload = {"text": text, "endpoint": "tokenize"}
+        response = self.request(payload)
+        request_id = response.get("request_id")
+        cost = response.get("costs")
+        outputs = response.get("outputs")[0]
+        return outputs, cost, request_id
